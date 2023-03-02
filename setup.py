8fd3da1df809e4586626a892130fe48d1af27c84
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-03-02 12:28:25 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+game_name = 'Splendor'
+time_run_game = 100
+N_AGENT = 5
+N_GAME = 15
+PASS_LEVEL = 2500
+COUNT_TRAIN = 10000
+COUNT_TEST = 1000
+
+# path = "C:\AutomaticTrain\State.xlsx"
+# SHOT_PATH = 'A:/AutoTrain/GAME/'
+DRIVE_FOLDER = 'G:/My Drive/AutomaticColab/'
+
+
+SHOT_PATH = ''
+# DRIVE_FOLDER = 'H:/Drive của tôi/AutomaticColab/'
