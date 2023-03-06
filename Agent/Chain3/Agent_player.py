46bfeac2a115b4fcee6bf6d101768f9cd7ef3089
AnnNguy3n <annguyen@vis-laboratory.com>
2023-03-06 15:11:27 +0700

    Sửa Chain3

--- Agent/Chain3/Agent_player.py
+++ Agent/Chain3/Agent_player.py
@@ -59,10 +59,10 @@ def generateArr(w):
 
 @njit
 def Train(state,per):
-    if getReward(state)!=-1:
-        if per[3][0][0]%100==0:
-            print(per[3][0][0])
-    if getActionSize()<200:
+    # if getReward(state)!=-1:
+    #     if per[3][0][0]%100==0:
+    #         print(per[3][0][0])
+    if getActionSize()<150:
         if per[2][0][0]==-1 or per[2][0][1]==-1:
             action = np.random.choice(np.where(getValidActions(state)==1)[0])
             if per[2][0][0]==-1:
@@ -129,7 +129,7 @@ def Train(state,per):
     
 @njit
 def Test(state,per):
-    if getActionSize()<200:
+    if getActionSize()<150:
         if per[2][0][0]==-1 or per[2][0][1]==-1:
             action = np.random.choice(np.where(getValidActions(state)==1)[0])
             if per[2][0][0]==-1:
