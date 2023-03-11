520e8299da21c9f011aa09c8647fe93adc700d32
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-03-11 17:40:51 +0700

    add splendor_v2, splendor_v3 lv1

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import Base.MachiKoro.env as env
-from CheckEnv import check_env
-print(check_env(env))
+# import Base.MachiKoro.env as env
+# from CheckEnv import check_env
+# print(check_env(env))
+
+import os
+for agent in os.listdir('Agent'):
+    if os.path.exists(f'Agent/{agent}/Data'):
+        for env_name in os.listdir(f'Agent/{agent}/Data'):
+            path_old =f'Agent/{agent}/Data/{env_name}' 
+            print(path_old.replace('0', '1'))
+            os.rename(path_old, path_old.replace('0', '1'))
