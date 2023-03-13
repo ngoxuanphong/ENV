75cd1d4443059991bf5429feb1ed2a1b74c3bd0b
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-03-13 20:19:42 +0700

    thêm hàm make

--- setup.py
+++ setup.py
@@ -13,3 +13,26 @@ DRIVE_FOLDER = 'G:/My Drive/AutomaticColab/'
 
 SHORT_PATH = ''
 # DRIVE_FOLDER = 'H:/Drive của tôi/AutomaticColab/'
+
+import sys, os
+from setup import SHORT_PATH
+import importlib.util
+game_name = 'Catan'
+
+def make(game_name):
+    def add_game_to_syspath(game_name):
+        if len(sys.argv) >= 2:
+            sys.argv = [sys.argv[0]]
+        sys.argv.append(game_name)
+
+    def setup_game(game_name):
+        spec = importlib.util.spec_from_file_location('env', f"{SHORT_PATH}Base/{game_name}/env.py")
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[spec.name] = module 
+        spec.loader.exec_module(module)
+        return module
+
+    add_game_to_syspath(game_name)
+    env = setup_game(game_name)
+
+    return env
--- test.py
+++ test.py
@@ -4,29 +4,8 @@
 # from CheckEnv import check_env
 # print(check_env(env))
 
-import numpy as np
-import random as rd
-from numba import njit, jit
-import sys, os
-from setup import SHORT_PATH
-import importlib.util
-game_name = 'Catan'
 
-def add_game_to_syspath(game_name):
-    if len(sys.argv) >= 2:
-        sys.argv = [sys.argv[0]]
-    sys.argv.append(game_name)
-
-def setup_game(game_name):
-    spec = importlib.util.spec_from_file_location('env', f"{SHORT_PATH}Base/{game_name}/env.py")
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[spec.name] = module 
-    spec.loader.exec_module(module)
-    return module
-
-add_game_to_syspath(game_name)
-env = setup_game(game_name)
-
+env = make('MachiKoro')
 getActionSize = env.getActionSize
 getStateSize = env.getStateSize
 getAgentSize = env.getAgentSize
@@ -36,6 +15,9 @@ getReward = env.getReward
 numba_main_2 = env.numba_main_2
 
 
+import numpy as np
+from numba import njit, jit
+from setup import make
 @njit()
 def bot_lv0(state, perData):
     validActions = getValidActions(state)
