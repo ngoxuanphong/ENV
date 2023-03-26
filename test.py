f821bf17d4a1913b04059f673d02be394aca16e4
ngoxuanphong <phong@vis-laboratory.com>
2023-03-26 08:20:16 +0700

    add data phom, imploding_kitten

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import numpy as np
 from numba import njit, jit
 import time
 
-env = make('Splendor')
+env = make('Phom')
 getActionSize = env.getActionSize
 getStateSize = env.getStateSize
 getAgentSize = env.getAgentSize
@@ -24,14 +24,14 @@ def bot_lv0(state, perData):
     idx = np.random.randint(0, arr_action.shape[0])
     return arr_action[idx], perData
 
-win1, per = numba_main_2(bot_lv0, 1000, np.array([0]), 0)
+win1, per = numba_main_2(bot_lv0, 100000, np.array([0]), 0)
 print(win1)
-a = time.process_time()
-win2, per = numba_main_2(bot_lv0, 1000, np.array([0]), 1)
-b = time.process_time()
-print(win2, b-a)
+# a = time.process_time()
+# win2, per = numba_main_2(bot_lv0, 1000, np.array([0]), 1)
+# b = time.process_time()
+# print(win2, b-a)
 
-a = time.process_time()
-win3, per = numba_main_2(bot_lv0, 1000, np.array([0]), -1)
-b = time.process_time()
-print(win3, b-a)
+# a = time.process_time()
+# win3, per = numba_main_2(bot_lv0, 1000, np.array([0]), -1)
+# b = time.process_time()
+# print(win3, b-a)
