91927b943539cea4e2d8074de679792b2788132c
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-04-01 15:01:01 +0700

    remove file thừa

--- test.py
+++ test.py
@@ -18,20 +18,20 @@ getReward = env.getReward
 numba_main_2 = env.numba_main_2
 
 @njit()
-def bot_lv0(state, perData):
+def Test(state, perData):
     validActions = getValidActions(state)
     arr_action = np.where(validActions==1)[0]
     idx = np.random.randint(0, arr_action.shape[0])
     return arr_action[idx], perData
 
-win1, per = numba_main_2(bot_lv0, 1000, np.array([0]), 0)
+win1, per = numba_main_2(Test, 1000, np.array([0]), 0)
 print(win1)
 # a = time.process_time()
-# win2, per = numba_main_2(bot_lv0, 1000, np.array([0]), 1)
+# win2, per = numba_main_2(Test, 1000, np.array([0]), 1)
 # b = time.process_time()
 # print(win2, b-a)
 
 a = time.process_time()
-win3, per = numba_main_2(bot_lv0, 1000, np.array([0]), -1)
+win3, per = numba_main_2(Test, 1000, np.array([0]), -1)
 b = time.process_time()
 print(win3, b-a)
