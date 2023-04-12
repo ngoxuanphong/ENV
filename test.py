13686c79a0644b8ac03f88c7ba6c92945163c492
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-04-12 20:17:20 +0700

    add game gofish, welcome to the dungeon v1, v2

--- test.py
+++ test.py
@@ -1,35 +1,35 @@
 # Check hệ thống
-# import Base.Phom.env as env
-# from CheckEnv import check_env
-# print(check_env(env))
+import Base.WelcomeToTheDungeon_v2.env as env
+from CheckEnv import check_env
+print(check_env(env))
 
-from setup import make
-import numpy as np
-from numba import njit, jit
-import time
+# from setup import make
+# import numpy as np
+# from numba import njit, jit
+# import time
 
-env = make('Phom')
-getActionSize = env.getActionSize
-getStateSize = env.getStateSize
-getAgentSize = env.getAgentSize
+# env = make('Phom')
+# getActionSize = env.getActionSize
+# getStateSize = env.getStateSize
+# getAgentSize = env.getAgentSize
 
-getValidActions = env.getValidActions
-getReward = env.getReward
-numba_main_2 = env.numba_main_2
+# getValidActions = env.getValidActions
+# getReward = env.getReward
+# numba_main_2 = env.numba_main_2
 
-@njit()
-def Test(state, perData):
-    validActions = getValidActions(state)
-    arr_action = np.where(validActions==1)[0]
-    idx = np.random.randint(0, arr_action.shape[0])
-    return arr_action[idx], perData
+# @njit()
+# def Test(state, perData):
+#     validActions = getValidActions(state)
+#     arr_action = np.where(validActions==1)[0]
+#     idx = np.random.randint(0, arr_action.shape[0])
+#     return arr_action[idx], perData
 
-win1, per = numba_main_2(Test, 1000, np.array([0]), 0)
-print(win1)
-a = time.process_time()
-win2, per = numba_main_2(Test, 1000, np.array([0]), 1)
-b = time.process_time()
-print(win2, b-a)
+# win1, per = numba_main_2(Test, 1000, np.array([0]), 0)
+# print(win1)
+# a = time.process_time()
+# win2, per = numba_main_2(Test, 1000, np.array([0]), 1)
+# b = time.process_time()
+# print(win2, b-a)
 
 # a = time.process_time()
 # win3, per = numba_main_2(Test, 1000, np.array([0]), -1)
