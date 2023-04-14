57e5ee3c9fb4108eb1bbf6a840c6482617ff4a3b
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-04-14 19:34:16 +0700

    add explotding kitten

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ import numpy as np
 from numba import njit, jit
 import time
 
-env = make('Fantan')
-print(check_env(env))
+env = make('Exploding_Kitten')
+# print(check_env(env))
 getActionSize = env.getActionSize
 getStateSize = env.getStateSize
 getAgentSize = env.getAgentSize
@@ -17,21 +17,21 @@ getValidActions = env.getValidActions
 getReward = env.getReward
 numba_main_2 = env.numba_main_2
 
-@njit()
+# @njit()
 def Test(state, perData):
     validActions = getValidActions(state)
     arr_action = np.where(validActions==1)[0]
     idx = np.random.randint(0, arr_action.shape[0])
     return arr_action[idx], perData
 
-win1, per = numba_main_2(Test, 1000, np.array([0]), 0)
-print(win1)
+# win1, per = numba_main_2(Test, 1000, np.array([0]), 0)
+# print(win1)
 a = time.process_time()
-win2, per = numba_main_2(Test, 1000, np.array([0]), 1)
+win2, per = numba_main_2(Test, 10000, np.array([0]), 1)
 b = time.process_time()
 print(win2, b-a)
 
 # a = time.process_time()
-# win3, per = numba_main_2(Test, 1000, np.array([0]), -1)
+# win3, per = numba_main_2(Test, 100000, np.array([0]), -1)
 # b = time.process_time()
 # print(win3, b-a)
