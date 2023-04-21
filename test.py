ddadbbdf40ddd01d7a9ebd253881bfecb813f9a1
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-04-21 22:50:41 +0700

    Sửa lại các game update đồ hoạ

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import numpy as np
 from numba import njit, jit
 import time
 
-env = make('Exploding_Kitten')
+env = make('Durak')
 print(check_env(env))
 getActionSize = env.getActionSize
 getStateSize = env.getStateSize
