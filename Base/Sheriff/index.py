74cce8662a655c6f5029e18ec406fc2b629a528c
AnnNguy3n <annguyen@vis-laboratory.com>
2023-05-11 11:40:22 +0700

    Sửa ht Sheriff

--- Base/Sheriff/index.py
+++ Base/Sheriff/index.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
-from numba import njit
+from numba import  njit
+
+
 
 
 ATTRIBUTE_PLAYER = 100
-"""
+'''
     OWNER: 100
         0: player coin
         1: debt
@@ -19,61 +21,61 @@ ATTRIBUTE_PLAYER = 100
         -45:-30: card_in_bag
         -30:-15: card_bag_drop
         -15: card_hand
-"""
-# (coin, debt, is_police, typy_bag(4), coin_bribe, number_smuggle_card, number_bribe_card_in_bag) => 10
-# 15*6: thẻ hối lộ done, thẻ done, thẻ hối lộ trong túi, thẻ trong túi, thẻ bỏ đi, thẻ trên tay => 90
+'''
+#(coin, debt, is_police, typy_bag(4), coin_bribe, number_smuggle_card, number_bribe_card_in_bag) => 10
+#15*6: thẻ hối lộ done, thẻ done, thẻ hối lộ trong túi, thẻ trong túi, thẻ bỏ đi, thẻ trên tay => 90
 OTHER_ATTRIBUTE_PLAYER = 29
-# (coin, debt, is_police, typy_bag(4), coin_bribe, number_smuggle_card, number_bribe_card_in_bag) => 10 vị trí
-# thẻ done hối lộ: 15 vị trí, thẻ done chính ngạch của người chơi: 4 vị trí => 19 vị trí
+#(coin, debt, is_police, typy_bag(4), coin_bribe, number_smuggle_card, number_bribe_card_in_bag) => 10 vị trí
+#thẻ done hối lộ: 15 vị trí, thẻ done chính ngạch của người chơi: 4 vị trí => 19 vị trí
 
 NUMBER_TYPE_CARD = 15
 CARD_OPEN_START = 5
 MAX_CARD_TAKE = 6
 NUMBER_PLAYER = 4
 NUMBER_CARD = 216
-NUMBER_CARD_USE = 186  # thẻ sau khi chia cho người chơi và bỏ ở 2 chồng bài
+NUMBER_CARD_USE = 186 #thẻ sau khi chia cho người chơi và bỏ ở 2 chồng bài
 NUMBER_CARD_OPEN = 125
 NUMBER_PHASE = 11
 
 INDEX = 0
-# Thông tin các người chơi
+#Thông tin các người chơi
 ENV_PLAYER_IN4 = INDEX
 INDEX += ATTRIBUTE_PLAYER * NUMBER_PLAYER
-# chồng bài úp
+#chồng bài úp
 ENV_DOWN_CARD = INDEX
 INDEX += NUMBER_CARD_USE
-# chồng bài lật trái
+#chồng bài lật trái
 ENV_LEFT_CARD = INDEX
 INDEX += NUMBER_CARD_OPEN
-# chồng bài lật phải
+#chồng bài lật phải
 ENV_RIGHT_CARD = INDEX
 INDEX += NUMBER_CARD_OPEN
-# thẻ người chơi bỏ trong lượt
+#thẻ người chơi bỏ trong lượt
 ENV_TEMP_DROP = INDEX
 INDEX += NUMBER_TYPE_CARD * NUMBER_PLAYER
-# Người chơi đang bị kiểm tra, chỉ sheriff nhìn
+#Người chơi đang bị kiểm tra, chỉ sheriff nhìn
 ENV_LAST_CHECKED = INDEX
 INDEX += NUMBER_PLAYER
-# người chơi đang hành động
+#người chơi đang hành động
 ENV_ID_ACTION = INDEX
 INDEX += 1
-# Số lượt đã done
+#Số lượt đã done
 ENV_ROUND = INDEX
 INDEX += 1
-# số người đã check
+#số người đã check
 ENV_NUMBER_CHECKED = INDEX
 INDEX += 1
-# check end game
+#check end game
 ENV_CHECK_END = INDEX
 INDEX += 1
-# Phase
+#Phase
 ENV_PHASE = INDEX
 INDEX += 1
 
 ENV_LENGTH = INDEX
 
 P_INDEX = 0
-# thông tin người chơi
+#thông tin người chơi
 P_PLAYER_IN4 = P_INDEX
 P_COIN = 0
 P_DEBT = 1
@@ -84,19 +86,19 @@ P_NUMBER_SMUGGLE = 8
 P_NUMBER_CARD_BRIBE_BAG = 9
 
 P_INDEX += ATTRIBUTE_PLAYER
-# thông tin các người chơi khác
+#thông tin các người chơi khác
 P_OTHER_PLAYER_IN4 = P_INDEX
 P_INDEX += OTHER_ATTRIBUTE_PLAYER * (NUMBER_PLAYER - 1)
-# 6 thẻ đầu ở chồng thẻ trái
+#6 thẻ đầu ở chồng thẻ trái
 P_LEFT_CARD = P_INDEX
 P_INDEX += NUMBER_TYPE_CARD * MAX_CARD_TAKE
-# 6 thẻ đầu ở chồng thẻ phải
+#6 thẻ đầu ở chồng thẻ phải
 P_RIGHT_CARD = P_INDEX
 P_INDEX += NUMBER_TYPE_CARD * MAX_CARD_TAKE
-# thẻ các người chơi khác đã bỏ
+#thẻ các người chơi khác đã bỏ
 P_CARD_OTHER_PLAYER_DROP = P_INDEX
 P_INDEX += NUMBER_TYPE_CARD * (NUMBER_PLAYER - 1)
-# Round
+#Round
 P_ROUND = P_INDEX
 P_INDEX += 1
 P_CHECK_END = P_INDEX
@@ -110,335 +112,47 @@ P_INDEX += NUMBER_TYPE_CARD * (NUMBER_PLAYER - 1)
 P_ORDER = P_INDEX
 P_INDEX += NUMBER_PLAYER
 
+P_NUMBER_CARD_IN_BAG = P_INDEX
+P_INDEX += NUMBER_PLAYER
+
 P_LENGTH = P_INDEX
+
+
+
+
+
 
 
 ALL_PENALTY = np.array([2, 2, 2, 2, 4, 4, 4, 4, 3, 4, 4, 4, 4, 5, 5])
 
 ALL_ORDER = np.arange(NUMBER_PLAYER)
 
-ALL_REWARD = np.array([2, 3, 3, 4, 6, 7, 8, 9, 4, 6, 6, 8, 6, 9, 9])
+ALL_REWARD = np.array([2, 3 ,3 ,4 ,6 ,7 ,8 ,9 ,4 ,6 ,6 ,8 ,6 ,9 ,9])
 
 ALL_NUMBER_COUNT = np.array([1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3])
 
-NORMAL_CARD = np.array(
-    [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        4,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        6,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        7,
-        8,
-        8,
-        8,
-        8,
-        8,
-    ]
-)
+NORMAL_CARD = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3,
+                        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                        4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                        5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                        7, 8, 8, 8, 8, 8])
 
-ROYAL_CARD = np.array([9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 15])
+ROYAL_CARD = np.array([ 9,  9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 15])
 
-START_PLAYER = np.array(
-    [
-        50.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-    ]
-)
+START_PLAYER = np.array([50., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
 
 
-"""
+'''
 Action	Mean
 0	bỏ thẻ apple
 1	bỏ thẻ cheese
@@ -522,4 +236,4 @@ Action	Mean
 79	Cho qua
 80	Bỏ thẻ tịch thu vào bên trái
 81	Bỏ thẻ tịch thu vào bên phải
-"""
+'''
