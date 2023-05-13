a792169c6d6438eb1b459592780f4dc843b7624e
Le Tan Dat <126872123+tandat17z@users.noreply.github.com>
2023-05-13 20:20:12 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ N_GAME = 15
 PASS_LEVEL = 2500
 COUNT_TRAIN = 10000
 COUNT_TEST = 1000
-
+# -------------------------------------------
 #  path = "C:\AutomaticTrain\State.xlsx"
 #  SHOT_PATH = 'A:/AutoTrain/GAME/'
 DRIVE_FOLDER = "G:/My Drive/AutomaticColab/"
