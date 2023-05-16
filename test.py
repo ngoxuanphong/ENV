9a9e24ee16c6bc1fe90c60d9dc10d0ac308b1095
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-05-16 15:35:10 +0700

    update wf file

--- test.py
+++ test.py
@@ -37,3 +37,6 @@ a = time.process_time()
 win3, per = numba_main_2(Test, 10000, np.array([0]), -1)
 b = time.process_time()
 print(win3, b - a)
+
+
+https://api.github.com/repos/ngoxuanphong/ngoxuanphong/ENV/commits/702115a7e09bb6dba60c4618f37dfe9fe6dbad70/files
