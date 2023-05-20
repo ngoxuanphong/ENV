7d1012f3a1941bace3548dee9c08de83d087235e
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-05-21 05:22:57 +0700

    use api get changed file

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -9,12 +9,7 @@ def get_changed_files():
     repo = Repo(repo_path)
     latest_commit = repo.head.commit
 
-    changed_files = []
-    for diff in latest_commit.diff(None):
-        changed_files.append(diff.a_path)
-    print((latest_commit.hexsha))
-
-    commit = "78b1588e67b36502797dd4a93f2929af9fd170dc"
+    commit = f"{latest_commit.hexsha}"
     api_path = f"https://api.github.com/repos/ngoxuanphong/ENV/commits/{commit}"
     command = f"curl -s {api_path}"
     output = subprocess.check_output(
@@ -22,22 +17,27 @@ def get_changed_files():
     ).decode("utf-8")
 
     output = json.loads(output)
+
+    print("Last commit: ", latest_commit.hexsha)
+    print("API:", api_path)
+    changed_files = []
     for file in output["files"]:
         print(file["filename"])
+        changed_files.append(file["filename"])
 
     return changed_files
 
 
 def test_print_name():
     changed_files = get_changed_files()
-    # for file in changed_files:
-    #     if "Base/" in file and "/_env.py" in file:
-    #         env_name = file.replace("Base/", "").replace("/_env.py", "")
-    #         print(env_name, "checking...")
-    #         check_env, list_bug = check_pytest(env_name)
-    #         if check_env == False:
-    #             print("ENV:", env_name, "FALSE:", list_bug)
-    #             assert False
-    #         else:
-    #             print("ENV:", env_name, "TRUE")
-    #             assert True
+    for file in changed_files:
+        if "Base/" in file and "/_env.py" in file:
+            env_name = file.replace("Base/", "").replace("/_env.py", "")
+            print(env_name, "checking...")
+            check_env, list_bug = check_pytest(env_name)
+            if check_env == False:
+                print("ENV:", env_name, "FALSE:", list_bug)
+                assert False
+            else:
+                print("ENV:", env_name, "TRUE")
+                assert True
