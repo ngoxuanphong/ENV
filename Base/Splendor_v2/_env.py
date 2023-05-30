ea62de963daaeeceec9d9e9001a73ce4b88bbb1e
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-05-30 18:14:52 +0700

    change if else path

--- Base/Splendor_v2/_env.py
+++ Base/Splendor_v2/_env.py
@@ -741,7 +741,7 @@ def load_module_player(player, game_name=None):
         )
     else:
         spec = importlib.util.spec_from_file_location(
-            "Agent_player", f"{SHORT_PATH}Agent/ifelse/{game_name}/{player}.py"
+            "Agent_player", f"{SHORT_PATH}Agent/Ifelse/{game_name}/{player}.py"
         )
     module = importlib.util.module_from_spec(spec)
     sys.modules[spec.name] = module
